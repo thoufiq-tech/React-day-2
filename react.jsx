117c5c953215c4b6e1aaf92314049c4ad6fea0e8
Thoufiq <110836719+thoufiq-tech@users.noreply.github.com>
2024-11-18 20:05:50 +0530

    Create react.jsx

---
+++ react.jsx
@@ -0,0 +1,63 @@
+export default function Card(){
+    return(
+        <>
+        <div className="container">
+        <div className="card">
+        <h6>FREE</h6>
+        <h1>$0/month</h1>
+        <hr />
+        <div className="font">
+        <i class="fa-solid fa-check"></i> Single User<br />
+        <i class="fa-solid fa-check"></i> 500 GB Storage <br />
+        <i class="fa-solid fa-check"></i> Unlimited Public Projects <br />
+        <i class="fa-solid fa-check"></i> Community Access <br />
+        <div className="font-color">
+        <i class="fa-regular fa-circle-xmark"></i> Unlimited Private Projects <br />
+        <i class="fa-regular fa-circle-xmark"></i> Dedicated Phone Support <br />
+        <i class="fa-regular fa-circle-xmark"></i> Free Subdomain <br />
+        <i class="fa-regular fa-circle-xmark"></i> Monthly Status Reports <br />
+        </div>
+        </div>
+        <br />
+        <button className="btn">BUTTON</button>
+    </div>
+    <div className="card">
+        <h6>PLUS</h6>
+        <h1>$9/month</h1>
+        <hr />
+        <div className="font">
+        <i class="fa-solid fa-check"></i> 5 Users<br />
+        <i class="fa-solid fa-check"></i> 50 GB Storage <br />
+        <i class="fa-solid fa-check"></i> Unlimited Public Projects <br />
+        <i class="fa-solid fa-check"></i> Community Access <br />
+        <i class="fa-solid fa-check"></i> Unlimited Private Projects <br />
+        <i class="fa-solid fa-check"></i> Dedicated Phone Support <br />
+        <i class="fa-solid fa-check"></i> Free Subdomain <br />
+        <div className="font-color">
+        <i class="fa-regular fa-circle-xmark"></i> Monthly Status Reports <br />
+        </div>
+        </div>
+        <br />
+        <button className="btn">BUTTON</button>
+    </div>
+    <div className="card">
+        <h6>PRO</h6>
+        <h1>$49/month</h1>
+        <hr />
+        <div className="font">
+        <i class="fa-solid fa-check"></i> Unlimited Users<br />
+        <i class="fa-solid fa-check"></i> 50 GB Storage <br />
+        <i class="fa-solid fa-check"></i> Unlimited Public Projects <br />
+        <i class="fa-solid fa-check"></i> Community Access <br />
+        <i class="fa-solid fa-check"></i> Unlimited Private Projects <br />
+        <i class="fa-solid fa-check"></i> Dedicated Phone Support <br />
+        <i class="fa-solid fa-check"></i> Free Subdomain <br />
+        <i class="fa-solid fa-check"></i> Monthly Status Reports <br />
+        </div>
+        <br />
+        <button className="btn">BUTTON</button>
+    </div>
+    </div>
+    </>
+    );
+}
